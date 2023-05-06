6744f20dc30ebd5a65f439e2d5f9e40c50da5dde
Le Hieu Nghia <lehieunghia2402@gmail.com>
2023-05-06 16:25:24 +0700

    Create project

---
+++ src/main/java/lehieunghia/spring/buoi2abai01/Buoi2ABai01Application.java
@@ -0,0 +1,13 @@
+package lehieunghia.spring.buoi2abai01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Buoi2ABai01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Buoi2ABai01Application.class, args);
+    }
+
+}
---
+++ src/test/java/lehieunghia/spring/buoi2abai01/Buoi2ABai01ApplicationTests.java
@@ -0,0 +1,13 @@
+package lehieunghia.spring.buoi2abai01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buoi2ABai01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
