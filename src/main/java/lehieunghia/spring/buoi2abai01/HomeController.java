a293064efa777df12c5e22f23154afdb30298e54
Le Hieu Nghia <lehieunghia2402@gmail.com>
2023-05-06 17:11:40 +0700

    Upload README.md

---
+++ src/main/java/lehieunghia/spring/buoi2abai01/HomeController.java
@@ -0,0 +1,22 @@
+package lehieunghia.spring.buoi2abai01;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.http.HttpRequest;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String Index(){
+        return "HomePage";
+    }
+    @RequestMapping("/display")
+    public String displayName(HttpServletRequest request){
+        String name = request.getParameter("name");
+        request.setAttribute("Name", name);
+        return "DisplayInformation";
+    }
+}
