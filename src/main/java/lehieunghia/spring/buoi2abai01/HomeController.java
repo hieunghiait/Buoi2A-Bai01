14541e498a1b597b22d92100d53498fb4d68b2dd
Le Hieu Nghia <lehieunghia2402@gmail.com>
2023-05-06 17:19:04 +0700

    Commit

--- src/main/java/lehieunghia/spring/buoi2abai01/HomeController.java
+++ src/main/java/lehieunghia/spring/buoi2abai01/HomeController.java
@@ -2,6 +2,7 @@ package lehieunghia.spring.buoi2abai01;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,7 +14,7 @@ public class HomeController {
     public String Index(){
         return "HomePage";
     }
-    @RequestMapping("/display")
+    @GetMapping("/display")
     public String displayName(HttpServletRequest request){
         String name = request.getParameter("name");
         request.setAttribute("Name", name);
